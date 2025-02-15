126fbf8a0146269e4e3b8f739f35435f54330180
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2025-02-14 23:05:03 -0500

    Ejericio 3 con POO

--- POO/p1/product.py
+++ POO/p1/product.py
@@ -41,9 +41,9 @@ class Product:
 
 
 class Pedido:
-    def __init__(self, products: Product, cantidad: int):
-        self.__products = products
-        self.__cuantity = cantidad
+    def __init__(self):
+        self.__products = []
+        self.__cuantity = []
     
     def total_pedido(self):
         total = 0
@@ -55,12 +55,43 @@ class Pedido:
         for (p, c) in zip(self.__products, self.__cuantity):
             print(f"Producto: {p.name}, valor: {p.price}. cantidad {c}")
     
+    #def add_produc(self, product: Product, cuantity: int): Esta es la forma correcta utilizada actualmente por seguir el curso utilizo la antigua
+    def add_produc(self, product: Product, cuantity: int):
+        if not isinstance(product, Product):
+            raise Exception('Product must be a Product type')
+        if cuantity <= 0:
+            raise Exception('Cuantity must be a positive value')
+        if product in self.__products:
+            indice = self.__products.index(product)
+            self.__cuantity[indice] += cuantity
+        else:
+            self.__products.append(product)
+            self.__cuantity.append(cuantity)
+    
+    def delete_product(self, product: Product):
+        if product in self.__products:
+            indice = self.__products.index(product)
+            del self.__products[indice]
+            del self.__cuantity[indice]
+        else:
+            raise Exception("Your product does not exits")
 
-p1 = Product(1, "Orthopedic Mattress",  500)
-p2 = Product(2, "Pillow",  25)
-p3 = Product(3, "Basement bed",  80)
-produc_list = [p1, p2, p3]
-cuantity = [2,4,6]
-pedido1 = Pedido(produc_list, cuantity)
-total = pedido1.total_pedido()
-print(total)
+    
+
+if __name__ == "__main__":
+    p1 = Product(1, "Orthopedic Mattress",  500)
+    p2 = Product(2, "Pillow",  25)
+    p3 = Product(3, "Basement bed",  80)
+    produc_list = [p1, p2, p3]
+    cuantity = [2,4,6]
+    #pedido1 = Pedido(produc_list, cuantity)
+    try:
+        pedido1 = Pedido()
+        pedido1.add_produc(p1, 5)
+        pedido1.add_produc(p2, 5)
+        pedido1.add_produc(p3, 5)
+        total = pedido1.total_pedido()
+        print(total)
+
+    except Exception as e:
+        print(e)
